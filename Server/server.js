f2516f3468c1f40dfc3424d88062520dcc07e286
Amit Manchanda <amitdelhi1995@gmail.com>
2015-10-13 23:12:50 +0530

    Added /takeoff and /land end points

--- Server/server.js
+++ Server/server.js
@@ -15,9 +15,40 @@ app.use(bodyParser.json());
 
 var port = process.env.PORT || 8080;        // set our port
 
+
 // ROUTES FOR OUR API
 // =============================================================================
 var router = express.Router();              // get an instance of the express Router
+
+// on routes that end in /takeoff
+// ----------------------------------------------------
+router.route('/takeoff')
+	//create a takeoff (accessed at POST http://localhost:8080/api/takoff)
+	.post(function(req, res){
+		var password = req.body.password;
+		if(password == "DroneFly")
+		{
+			console.log("Takeoff");
+			res.json({message : "Aaye aaye! Captain."});
+		}
+		
+	});
+
+
+// on routes that end in /land
+// ----------------------------------------------------
+router.route('/land')
+        //create a land (accessed at POST http://localhost:8080/api/land)
+        .post(function(req, res){
+                var password = req.body.password;
+                if(password == "DroneFly")
+                {
+			console.log("Land");
+                        res.json({message : "Fasten your Seat belts. We are going down."});
+                }
+
+        });
+
 
 // test route to make sure everything is working (accessed at GET http://localhost:8080/api)
 router.get('/', function(req, res) {
