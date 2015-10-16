2284206441a3a0072bb86487f37d99da10325394
Abhinav Kaushal Keshari <keshariabhinavk1996@gmail.com>
2015-10-16 17:02:25 +0530

    added message for get request

--- Server/server.js
+++ Server/server.js
@@ -30,7 +30,7 @@ var client_arDrone = arDrone.createClient();
 // on routes that end in /takeoff
 // ----------------------------------------------------
 router.route('/takeoff')
-	//create a takeoff (accessed at POST http://localhost:8080/api/takoff)
+	//create a takeoff (accessed at POST http://localhost:8080/api/takeoff)
 	.post(function(req, res){
 		var password = req.body.password;
 		if(password == "DroneFly")
@@ -72,7 +72,8 @@ router.route('/land')
 
 // test route to make sure everything is working (accessed at GET http://localhost:8080/api)
 router.get('/', function(req, res) {
-    res.json({ message: 'hooray! welcome to our api!' });   
+    res.json({ message: 'hooray! welcome to our api!' });
+    console.log("Someone visited. Open the gates");
     });
 
 // more routes for our API will happen here
